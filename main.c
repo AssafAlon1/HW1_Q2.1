143cf98e0f9a00467b24c7a7c7f65a14dc63554a
AssafAlon1 <alon.assaf@campus.technion.ac.il>
2021-05-06 16:43:16 +0300

    beta 2

--- main.c
+++ main.c
@@ -72,7 +72,6 @@ bool isListSorted(Node list)
     }
 
     // Reached NULL, the list is sorted
-    printf("list is sorted\n");
     return true;
 }
 
@@ -144,17 +143,16 @@ bool addAllElementsToNewList(Node old_list, Node new_list, ErrorCode *error_code
     return true;
 }
 
-// Merges two sorted lists into a new one, returning it. Error codes will be provided through the ErrorCode variable 
-Node mergeSortedLists(Node list1, Node list2, ErrorCode* error_code)
+// Verify the user has supplied valid input
+bool verifyValidInput (Node list1, Node list2, ErrorCode* error_code)
 {
-
     // Verify error_code is a valid pointer
     if (error_code == NULL)
     {
         // can't update error_code :(
-        return NULL;
+        return false;
     }
-    
+
     // Verify the lists aren't empty
     int list1_length = getListLength(list1);    
     int list2_length = getListLength(list2);
@@ -162,15 +160,30 @@ Node mergeSortedLists(Node list1, Node list2, ErrorCode* error_code)
     if (list1_length == 0 || list2_length == 0)
     {
         *error_code = NULL_ARGUMENT;
-        return NULL;
+        return false;
     }
-    
+
     // Verify the lists are sorted
     if (isListSorted(list1) != true || isListSorted(list2) != true)
     {
         *error_code = UNSORTED_LIST;
+        return false;
+    }
+
+    // Input is valid
+    return true;
+}
+
+// Merges two sorted lists into a new one, returning it. Error codes will be provided through the ErrorCode variable 
+Node mergeSortedLists(Node list1, Node list2, ErrorCode* error_code)
+{
+    
+    // Verifies the input is valid. If it's invalid, the error_code will be updated
+    if (verifyValidInput(list1, list2, error_code) == false)
+    {
         return NULL;
     }
+    
 
     // Memory allocation for new list - allocate 1 node at a time
     Node new_list = allocateNextNode(NULL);
@@ -212,7 +225,7 @@ Node mergeSortedLists(Node list1, Node list2, ErrorCode* error_code)
         current_node       = next_node;
     }
 
-    // 1 list still has elements - fi
+    // 1 list still has elements
     Node list_remainer = list1 == NULL ? list2 : list1;
     bool add_elements_result = addAllElementsToNewList(list_remainer, current_node, error_code);
     if (add_elements_result == false)
@@ -255,7 +268,7 @@ int main()
             if (input == -2)
             {
                 current_list++;
-                if (current_list == 1)
+                if (current_list == 2)
                 {
                     freeFullList(list1);
                     list1 = NULL;
